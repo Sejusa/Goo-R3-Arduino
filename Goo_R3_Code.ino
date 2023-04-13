3344ad27b01f9dd39d41ae5d5f58da5130c6db96
Sejusa <110556472+Sejusa@users.noreply.github.com>
2023-04-13 12:14:31 +0200

    Versión Alfa 1
    Versión Alfa 1 del proyecto Goo R3 (robot aspirador) hecho con Arduino.

---
+++ Goo_R3_Code.ino
@@ -0,0 +1,27 @@
+//Proyecto Arduino Goo R3
+// Hecho por Sergi Funes, Marcos Cobos y Cristhian Rodríguez
+
+int interruptor = 0;
+int ledEncendido = 1;
+int motor = 2;
+
+void setup() 
+{
+  pinMode(interruptor, INPUT);
+  pinMode(ledEncendido, OUTPUT);
+  pinMode(motor, OUTPUT);
+}
+
+void loop() 
+{
+  while (digitalRead(interruptor) == HIGH)
+  {
+    digitalWrite(ledEncendido, HIGH);
+    digitalWrite(motor, HIGH);
+    if (digitalRead(interruptor) == LOW)
+    {
+      digitalWrite(ledEncendido, LOW);
+      digitalWrite(motor, LOW);
+    }
+  }
+}
